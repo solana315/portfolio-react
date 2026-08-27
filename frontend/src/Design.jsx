6802d98c1765b3af5650b0441fa135e3d60bd28b
ANA3501 <pv27649@alunos.estgv.ipv.pt>
2026-08-27 11:03:06 +0100

    .

--- frontend/src/Design.jsx
+++ frontend/src/Design.jsx
@@ -10,6 +10,9 @@ import Artboard1 from './assets/Artboard1.png';
 import Ivaristo1 from './assets/Ivaristo/Ivaristo1.jpg';
 import Ivaristo3 from './assets/Ivaristo/Ivaristo3.jpg';
 import postcard from './assets/Ivaristo/postcard.jpg';
+import Ivaristo5 from './assets/Ivaristo/Ivaristo5.png';
+import Ivaristo7 from './assets/Ivaristo/Ivaristo7.png';
+import Ivaristo4 from './assets/Ivaristo/Ivaristo4.png';
 
 
 
@@ -53,7 +56,6 @@ function Design() {
                         src={postcard}
                         alt="Referência 2"
                         rotate={4}
-                        caption="PostCards"
                     />
                 </Reveal>
 
@@ -65,24 +67,51 @@ function Design() {
                     </h2>
                 </Reveal>
 
+                
+{/*luminouscard*/}
                 <Reveal variant="rotate-in" delay={200} className="scrap-item pos-5">
                     <ScrapbookPhoto
                         src={placarIvaristo}
-                        alt="Referência 1"
-                        rotate={-6}
-                        circled
+                        alt="Referência 3"
+                        rotate={-6} 
                     />
                 </Reveal>
 
 
-
+{ /* coffee*/}
                 <Reveal variant="rotate-in" delay={0} className="scrap-item pos-6">
                     <ScrapbookPhoto
                         src={Ivaristo1}
                         alt="Referência 4"
-                        rotate={16}
+                        rotate={6}
                     />
                 </Reveal>
+
+{ /* eyeswall*/}
+                <Reveal variant="rotate-in" delay={0} className="scrap-item pos-7">
+                    <ScrapbookPhoto
+                        src={Ivaristo5}
+                        alt="Referência 5"
+                        rotate={-3}
+                    />
+                </Reveal>
+{ /* book*/}
+                <Reveal variant="rotate-in" delay={0} className="scrap-item pos-8">
+                    <ScrapbookPhoto
+                        src={Ivaristo7}
+                        alt="Referência 6"
+                        rotate={-8}
+                    />
+                </Reveal>
+{ /* sweatshirt*/}
+                 <Reveal variant="rotate-in" delay={0} className="scrap-item pos-9">
+                    <ScrapbookPhoto
+                        src={Ivaristo4}
+                        alt="Referência 7"
+                        rotate={3}
+                    />
+                </Reveal>
+
 
             </section>
 
