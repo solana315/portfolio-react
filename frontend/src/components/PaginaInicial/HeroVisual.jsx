f8bfb96fe51f066e16ef07333ab6b46fb5f22d73
solana315 <pv27649@alunos.estgv.ipv.pt>
2026-09-05 20:58:49 +0100

    ,

--- frontend/src/components/PaginaInicial/HeroVisual.jsx
+++ frontend/src/components/PaginaInicial/HeroVisual.jsx
@@ -1,15 +1,32 @@
+import Foto from '../../assets/Foto.jpeg';
+import FotoDois from '../../assets/FotoDois.jpeg';
+import FotoTres from '../../assets/FotoTres.jpeg';
+import FotoQuatro from '../../assets/FotoQuatro.jpeg';
+import FotoCinco from '../../assets/FotoCinco.jpeg';
+import FotoSeis from '../../assets/FotoSeis.jpeg';
+import FotoSete from '../../assets/FotoSete.jpeg';
+import FotoOito from '../../assets/FotoOito.jpeg';
+import FotoNove from '../../assets/FotoNove.jpeg';
+import FotoDez from '../../assets/FotoDez.jpeg';
+import FotoOnze from '../../assets/FotoOnze.jpeg';
+import FotoDoze from '../../assets/FotoDoze.jpeg';
+import FotoTreze from '../../assets/FotoTreze.jpeg';
+
+
 const galleryTiles = [
-  { key: 'paper', className: 'tile tile--paper', style: { gridColumn: '1 / span 2', gridRow: '1 / span 2' } },
-  { key: 'pink', className: 'tile tile--pink', style: { gridColumn: '3 / span 1', gridRow: '1 / span 1' } },
-  { key: 'paper', className: 'tile tile--paper', style: { gridColumn: '4 / span 1', gridRow: '1 / span 1' } },
-  { key: 'green', className: 'tile tile--green', style: { gridColumn: '3 / span 2', gridRow: '2 / span 2' } },
-  { key: 'pink', className: 'tile tile--pink', style: { gridColumn: '1 / span 1', gridRow: '3 / span 1' } },
-  { key: 'green', className: 'tile tile--green', style: { gridColumn: '2 / span 1', gridRow: '3 / span 1' } },
-  { key: 'paper', className: 'tile tile--paper', style: { gridColumn: '1 / span 2', gridRow: '4 / span 1' } },
-  { key: 'pink', className: 'tile tile--pink', style: { gridColumn: '3 / span 2', gridRow: '4 / span 1' } },
-  { key: 'green', className: 'tile tile--green', style: { gridColumn: '1 / span 1', gridRow: '5 / span 1' } },
-  { key: 'paper', className: 'tile tile--paper', style: { gridColumn: '2 / span 1', gridRow: '5 / span 1' } },
-  { key: 'pink', className: 'tile tile--pink', style: { gridColumn: '3 / span 2', gridRow: '5 / span 1' } },
+  { key: 'fotoCinco', img: FotoCinco, style: { gridColumn: '1 / span 2', gridRow: '1 / span 2' } },
+  { key: 'fotoDoze', img: FotoDoze, style: { gridColumn: '3 / span 1', gridRow: '1 / span 1' } },
+  { key: 'fotoOnze', img: FotoOnze, style: { gridColumn: '4 / span 1', gridRow: '1 / span 1' } },
+  { key: 'fotoDez', img: FotoDez, style: { gridColumn: '3 / span 2', gridRow: '2 / span 2' } },
+  { key: 'fotoNove', img: FotoNove, style: { gridColumn: '1 / span 1', gridRow: '3 / span 1' } },
+  { key: 'fotoOito', img: FotoOito, style: { gridColumn: '2 / span 1', gridRow: '3 / span 1' } },
+  { key: 'fotoSete', img: FotoSete, style: { gridColumn: '1 / span 2', gridRow: '4 / span 1' } },
+  { key: 'fotoTreze', img: FotoTreze, style: { gridColumn: '3 / span 2', gridRow: '4 / span 1' } },
+  { key: 'fotoQuatro', img: FotoQuatro, style: { gridColumn: '1 / span 1', gridRow: '5 / span 1' } },
+  { key: 'foto', img: Foto, style: { gridColumn: '2 / span 1', gridRow: '5 / span 1' } },
+  { key: 'fotoDois', img: FotoDois, style: { gridColumn: '3 / span 2', gridRow: '5 / span 1' } },
+  { key: 'fotoTres', img: FotoTres, style: { gridColumn: '1 / span 2', gridRow: '6 / span 1' } },
+  { key: 'fotoSeis', img: FotoSeis, style: { gridColumn: '3 / span 2', gridRow: '6 / span 1' } },
 ];
 
 const HeroVisual = () => (
@@ -17,7 +34,15 @@ const HeroVisual = () => (
     <div className="collage-shell position-relative">
       <div className="collage-grid">
         {galleryTiles.map((tile) => (
-          <div key={tile.key} className={tile.className} style={tile.style} />
+          <div
+            key={tile.key}
+            className={tile.img ? 'tile tile--photo' : tile.className}
+            style={
+              tile.img
+                ? { ...tile.style, backgroundImage: `url(${tile.img})` }
+                : tile.style
+            }
+          />
         ))}
       </div>
 
